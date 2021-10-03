b967e5b7dccfb7c4aebda3d1c380c28d999afdcf
岩本崚汰 <lengtaiyanben@gmail.com>
2021-10-03 12:32:36 +0900

    statelessWidgetに変更

---
+++ lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:dynamic_ui_enable_image_add/first/first_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstPage(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dynamic_ui_enable_image_add/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:dynamic_ui_enable_image_add/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
